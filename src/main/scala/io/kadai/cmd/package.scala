0aa22521c3282b88003b246fa5921989b3e6beba
Jed Wesley-Smith <jed@atlassian.com>
2012-12-26 16:21:02 +1100

    generalised Traversable optional syntax

--- src/main/scala/io/kadai/cmd/package.scala
+++ src/main/scala/io/kadai/cmd/package.scala
@@ -1,11 +1,30 @@
 package io.kadai
 
+import collection.GenTraversableLike
+import collection.generic.IsTraversableLike
+
 package object cmd {
-  private[cmd] def tailOption[A](as: Seq[A]):Option[Seq[A]] =
-    if (as.isEmpty) None
-    else {
-      val tail = as.tail
-      if (tail.isEmpty) None
-      else Some(tail)
-    }
+
+  implicit class TraversableOptionalSyntax[Repr <: GenTraversableLike[_, Repr]](val r: Repr) extends AnyVal {
+    //type A = ???
+    def notEmpty: Option[Repr] =
+      if (r.isEmpty) None else Some(r)
+
+    def tailOption: Option[Repr] =
+      notEmpty flatMap { r => new TraversableOptionalSyntax(r.tail).notEmpty }
+
+    // experiment, needs to be: (A, Repr) currently: (Any, Repr)
+    def headTailOption: Option[(Any, Repr)] =
+       notEmpty map { r => r.head -> r.tail }
+
+    private def map[A](so: => A): Option[A] =
+      if (r.isEmpty) None else Some(so)
+
+    private def flatMap[A](so: => Option[A]): Option[A] =
+      if (r.isEmpty) None else so
+  }
+
+  // somehow we need to attempt to capture the IsTraversableLike's head type
+  //implicit def ToTailOption[A, R<: GenTraversableLike[_, R]](r: R)(implicit fr: IsTraversableLike[R]): TailOption[fr.A, R] =
+  //  new TailOption(r)
 }
