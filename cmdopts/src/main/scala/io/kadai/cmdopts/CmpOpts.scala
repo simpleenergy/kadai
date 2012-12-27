db6c74f4f5da382bcfedd4b508fd849ea7c1ec53
Jed Wesley-Smith <jed@atlassian.com>
2012-12-27 14:33:42 +1100

    first cut of the componentisation

--- cmdopts/src/main/scala/io/kadai/cmdopts/CmpOpts.scala
+++ cmdopts/src/main/scala/io/kadai/cmdopts/CmpOpts.scala
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 package io.kadai
-package cmd
+package cmdopts
 
 import shapeless._
 import scalaz._
