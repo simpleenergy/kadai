1cdf700212d342d9519fc3639bf51b2f20e12194
Jed Wesley-Smith <jed@atlassian.com>
2013-01-02 17:41:19 +1100

    added the HostConverter from soke

---
+++ logging/src/main/scala/io/kadai/log/log4j/HostConverter.scala
@@ -0,0 +1,30 @@
+package io.kadai.log.log4j
+
+import java.net.InetAddress;
+
+import org.apache.logging.log4j.core
+import core.config.plugins.Plugin
+import core.pattern.{ ConverterKeys, LogEventPatternConverter }
+
+/** Adds the host name as a log element, use %host in the pattern to access.
+ */
+object HostConverter {
+  def newInstance(options: Array[String]): HostConverter = {
+    val numberOfElements: Int = options.headOption.map { _.toInt } getOrElse 1
+
+    new HostConverter(
+      InetAddress.getLocalHost.getHostName.
+        split("\\.").
+        take(numberOfElements).
+        mkString(".")
+    )
+  }
+}
+
+@Plugin(name = "host", `type` = "Converter")
+@ConverterKeys(Array("host"))
+class HostConverter(host: String) extends LogEventPatternConverter("hostname", "") {
+  def format(e: core.LogEvent, sb: java.lang.StringBuilder) {
+    sb.append(host);
+  }
+}
