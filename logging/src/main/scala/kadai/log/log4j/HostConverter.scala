e72aebb2a1e70b8ec73adcebe757e1ecd5cf6e4f
Jed Wesley-Smith <jed@atlassian.com>
2013-05-25 11:52:07 +1000

    update to a version of log4j that doesn't have serious races in its configuration

--- logging/src/main/scala/kadai/log/log4j/HostConverter.scala
+++ logging/src/main/scala/kadai/log/log4j/HostConverter.scala
@@ -1,19 +1,18 @@
 package kadai.log.log4j
 
-import java.net.InetAddress;
-
 import org.apache.logging.log4j.core
 import core.config.plugins.Plugin
 import core.pattern.{ ConverterKeys, LogEventPatternConverter }
 
-/** Adds the host name as a log element, use %host in the pattern to access.
+/**
+ * Adds the host name as a log element, use %host in the pattern to access.
  */
 object HostConverter {
   def newInstance(options: Array[String]): HostConverter = {
     val numberOfElements: Int = options.headOption.map { _.toInt } getOrElse 1
 
     new HostConverter(
-      InetAddress.getLocalHost.getHostName.
+      java.net.InetAddress.getLocalHost.getHostName.
         split("\\.").
         take(numberOfElements).
         mkString(".")
@@ -21,7 +20,7 @@ object HostConverter {
   }
 }
 
-@Plugin(name = "host", `type` = "Converter")
+@Plugin(name = "host", category = "Converter")
 @ConverterKeys(Array("host"))
 class HostConverter(host: String) extends LogEventPatternConverter("hostname", "") {
   def format(e: core.LogEvent, sb: java.lang.StringBuilder) {
