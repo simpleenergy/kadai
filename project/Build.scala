812db48140fe40fa2bc2c670bf402c4b011a523d
Jed Wesley-Smith <jed@atlassian.com>
2013-06-05 14:04:08 +1000

    releasing 0.0.5-RC4

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,7 @@
 import sbt._, Keys._
 
 object KadaiBuild extends Build {
-  lazy val projectVersion = "0.0.5-RC3"
+  lazy val projectVersion = "0.0.5-RC4"
 
   lazy val mavenLocal = Some(Resolver.file("file",  new File(Path.userHome.absolutePath+"/.m2/repository")))
 
