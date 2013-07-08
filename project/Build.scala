d9a154c70911f95a92cf184f7d8941143e76ae27
Jed Wesley-Smith <jed@atlassian.com>
2013-07-08 15:17:24 +1000

    preparing for next development iteration

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,7 @@
 import sbt._, Keys._
 
 object KadaiBuild extends Build {
-  lazy val projectVersion = "1.0.0"
+  lazy val projectVersion = "1.1.0-SNAPSHOT"
 
   lazy val mavenLocal = Some(Resolver.file("file",  new File(Path.userHome.absolutePath+"/.m2/repository")))
 
