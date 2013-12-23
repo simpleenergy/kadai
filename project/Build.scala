7538775ca9f200017f4e13d76933c940322896b5
Jed Wesley-Smith <jed@atlassian.com>
2013-12-23 11:04:36 +1100

    preparing release, added the sbt-release plugin

--- project/Build.scala
+++ project/Build.scala
@@ -13,56 +13,58 @@
 // limitations under the License.
 
 import sbt._, Keys._
+import sbtrelease.ReleasePlugin._
 
 object KadaiBuild extends Build {
   lazy val projectVersion = "1.1.0-SNAPSHOT"
 
   lazy val mavenLocal = Some(Resolver.file("file",  new File(Path.userHome.absolutePath+"/.m2/repository")))
 
-  lazy val standardSettings = Defaults.defaultSettings ++ Seq[Project.Setting[_]] (
-    organization := "io.kadai"
-  , version := projectVersion
-  , licenses := Seq("Apache2" -> url("https://bitbucket.org/atlassian/kadai/raw/master/LICENSE"))
-  , homepage := Some(url("https://bitbucket.org/atlassian/kadai"))
-  , pomExtra := (
-        <scm>
-            <url>git@bitbucket.org:atlassian/kadai.git</url>
-            <connection>scm:git:git@bitbucket.org:atlassian/kadai.git</connection>
-            <developerConnection>scm:git:git@bitbucket.org:atlassian/kadai.git</developerConnection>
-        </scm>
-        <distributionManagement>
-            <repository>
-                <id>atlassian-public</id>
-                <name>Atlassian Public Repository</name>
-                <url>https://maven.atlassian.com/public</url>
-            </repository>
-            <snapshotRepository>
-                <id>atlassian-public-snapshot</id>
-                <name>Atlassian Public Snapshot Repository</name>
-                <url>https://maven.atlassian.com/public-snapshot</url>
-            </snapshotRepository>
-        </distributionManagement>
+  lazy val standardSettings = 
+    Defaults.defaultSettings ++ 
+    releaseSettings ++ // sbt-release
+    net.virtualvoid.sbt.graph.Plugin.graphSettings ++ // dependency plugin settings 
+    Seq[Project.Setting[_]] (
+      organization := "io.kadai"
+    , version := projectVersion
+    , licenses := Seq("Apache2" -> url("https://bitbucket.org/atlassian/kadai/raw/master/LICENSE"))
+    , homepage := Some(url("https://bitbucket.org/atlassian/kadai"))
+    , pomExtra := (
+          <scm>
+              <url>git@bitbucket.org:atlassian/kadai.git</url>
+              <connection>scm:git:git@bitbucket.org:atlassian/kadai.git</connection>
+              <developerConnection>scm:git:git@bitbucket.org:atlassian/kadai.git</developerConnection>
+          </scm>
+          <distributionManagement>
+              <repository>
+                  <id>atlassian-public</id>
+                  <name>Atlassian Public Repository</name>
+                  <url>https://maven.atlassian.com/public</url>
+              </repository>
+              <snapshotRepository>
+                  <id>atlassian-public-snapshot</id>
+                  <name>Atlassian Public Snapshot Repository</name>
+                  <url>https://maven.atlassian.com/public-snapshot</url>
+              </snapshotRepository>
+          </distributionManagement>
+      )
+    , pomIncludeRepository := { (repo: MavenRepository) => false } // no repositories in the pom
+    , scalaVersion := "2.10.3"
+    , scalacOptions := Seq("-deprecation", "-unchecked", "-feature", "-language:_", "-Ywarn-value-discard",  "-Xfatal-warnings", "-Xlog-free-terms")
+    , javacOptions ++= Seq("-encoding", "UTF-8")
+    , resolvers ++= Seq(
+        "Tools Snapshots"  at "http://oss.sonatype.org/content/repositories/snapshots"
+      , "Tools Releases"   at "http://oss.sonatype.org/content/repositories/releases"
+      , "atlassian-public" at "https://maven.atlassian.com/content/groups/atlassian-public/"
+      , "atlassian-internal" at "https://maven.atlassian.com/content/groups/internal/"
+      )
+    , mappings in (Compile, packageBin) ++= Seq(
+        file("LICENSE") -> "META-INF/LICENSE"
+      , file("NOTICE")  -> "META-INF/NOTICE"
+      )
+    , credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
+    , addCompilerPlugin("org.scala-lang.plugins" % "macro-paradise" % "2.0.0-SNAPSHOT" cross CrossVersion.full)
     )
-  , pomIncludeRepository := { (repo: MavenRepository) => false } // no repositories in the pom
-  , scalaVersion := "2.10.3"
-  , scalacOptions := Seq("-deprecation", "-unchecked", "-feature", "-language:_", "-Ywarn-value-discard",  "-Xfatal-warnings", "-Xlog-free-terms")
-  , javacOptions ++= Seq("-encoding", "UTF-8")
-  , resolvers ++= Seq(
-      "Tools Snapshots"  at "http://oss.sonatype.org/content/repositories/snapshots"
-    , "Tools Releases"   at "http://oss.sonatype.org/content/repositories/releases"
-    , "atlassian-public" at "https://maven.atlassian.com/content/groups/atlassian-public/"
-    , "atlassian-internal" at "https://maven.atlassian.com/content/groups/internal/"
-      // Contegix, m2proxy.atlassian.com is borked, m2proxy-int.private.atlassian.com works
-    //, "atlassian-public-ctx" at "http://m2proxy-int.private.atlassian.com/content/groups/atlassian-public/"
-    //, "atlassian-internal-ctx" at "http://m2proxy-int.private.atlassian.com/content/groups/internal/"
-    )
-  , mappings in (Compile, packageBin) ++= Seq(
-      file("LICENSE") -> "META-INF/LICENSE"
-    , file("NOTICE")  -> "META-INF/NOTICE"
-    )
-  , credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
-  , addCompilerPlugin("org.scala-lang.plugins" % "macro-paradise" % "2.0.0-SNAPSHOT" cross CrossVersion.full)
-  ) ++ net.virtualvoid.sbt.graph.Plugin.graphSettings // add dependency plugin settings
 
   lazy val core = Project(id = "core"
   , base = file("core")
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,5 @@
 addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.2.0")
 
 addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.4")
+
+addSbtPlugin("com.github.gseitz" % "sbt-release" % "0.8")
